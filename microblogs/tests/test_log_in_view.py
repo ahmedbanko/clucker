c196981ef6176d1f729cbe09d3febc57465ae38f
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2021-10-21 11:15:09 +0100

    Log in user after succesful sign up

--- microblogs/tests/test_log_in_view.py
+++ microblogs/tests/test_log_in_view.py
@@ -2,11 +2,21 @@
 from django.test import TestCase
 from django.urls import reverse
 from microblogs.forms import LogInForm
+from microblogs.models import User
+from .helpers import LogInTester
 
-class LogInViewTestCase(TestCase):
+class LogInViewTestCase(TestCase, LogInTester):
 
     def setUp(self):
         self.url = reverse('log_in')
+        User.objects.create_user(
+            '@johndoe',
+            first_name = 'John',
+            last_name = 'Doe',
+            email = 'johndoe@example.org',
+            password = 'Password123',
+            bio = 'The quick brown fox jumps over the lazy dog.',
+        )
 
 
     def test_log_in_url(self):
@@ -21,31 +31,21 @@ class LogInViewTestCase(TestCase):
         self.assertFalse(form.is_bound)
 
 
-    # def test_unsuccesful_log_in(self):
-    #     self.form_input['username'] = 'BAD_USERNAME'
-    #     before_count = User.objects.count()
-    #     response = self.client.post(self.url, self.form_input)
-    #     after_count = User.objects.count()
-    #     self.assertEqual(after_count, before_count)
-    #     self.assertEqual(response.status_code, 200)
-    #     self.assertTemplateUsed(response, 'sign_up.html')
-    #     form = response.context['form']
-    #     self.assertTrue(isinstance(form, SignUpForm))
-    #     self.assertTrue(form.is_bound)
-    #
-    #
-    # def test_succesful_sign_up(self):
-    #     before_count = User.objects.count()
-    #     response = self.client.post(self.url, self.form_input, follow = True)
-    #     after_count = User.objects.count()
-    #     self.assertEqual(after_count, before_count+1)
-    #     response_url = reverse('feed')
-    #     self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
-    #     self.assertTemplateUsed(response, 'feed.html')
-    #     user = User.objects.get(username = '@janedoe')
-    #     self.assertEqual(user.first_name, 'Jane')
-    #     self.assertEqual(user.last_name, 'Doe')
-    #     self.assertEqual(user.email, 'janedoe@example.org')
-    #     self.assertEqual(user.bio, 'Jane bio')
-    #     is_password_correct = check_password('Password123', user.password)
-    #     self.assertTrue(is_password_correct)
+    def test_unsuccesful_log_in(self):
+        form_input = {'username' : '@johndoe', 'password' : 'WrongPassword123'}
+        response = self.client.post(self.url, form_input)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'log_in.html')
+        form = response.context['form']
+        self.assertTrue(isinstance(form, LogInForm))
+        self.assertFalse(form.is_bound)
+        self.assertFalse(self._is_logged_in())
+
+
+    def test_succesful_log_in(self):
+        form_input = {'username' : '@johndoe', 'password' : 'Password123'}
+        response = self.client.post(self.url, form_input, follow = True)
+        self.assertTrue(self._is_logged_in())
+        response_url = reverse('feed')
+        self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
+        self.assertTemplateUsed(response, 'feed.html')
--- microblogs/views.py
+++ microblogs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import SignUpForm, LogInForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'home.html')
@@ -8,7 +9,8 @@ def sign_up(request):
     if request.method == 'POST':
         form = SignUpForm(request.POST)
         if form.is_valid():
-            form.save()
+            user = form.save()
+            login(request, user)
             return redirect('feed')
     else:
         form = SignUpForm()
@@ -18,5 +20,14 @@ def feed(request):
     return render(request, 'feed.html')
 
 def log_in(request):
+    if request.method == 'POST':
+        form = LogInForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(username =  username, password = password)
+            if user is not None:
+                login(request, user)
+                return redirect('feed')
     form = LogInForm()
     return render(request, 'log_in.html', {'form' : form})
