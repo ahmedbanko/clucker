8c68d67ffea08bcb45046325cb7a66c9f0a3bedf
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2021-10-21 10:03:56 +0100

    Add LogInForm with tests

--- microblogs/forms.py
+++ microblogs/forms.py
@@ -40,3 +40,8 @@ class SignUpForm(forms.ModelForm):
                     password = self.cleaned_data.get('new_password'),
         )
         return user
+
+
+class LogInForm(forms.Form):
+    username = forms.CharField(label = 'Username')
+    password = forms.CharField(label = 'Password', widget =  forms.PasswordInput())
