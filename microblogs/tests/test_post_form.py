f26113277620011b105d610e264f563bf15c227d
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2021-11-06 10:33:27 +0000

    Add login view test for blank username and password

--- microblogs/tests/test_post_form.py
+++ microblogs/tests/test_post_form.py
@@ -2,12 +2,13 @@ from django.test import TestCase
 from django import forms
 from microblogs.models import User, Post
 from microblogs.forms import PostForm
-
+from django.urls import reverse
 
 """ Unit test for the post form"""
 class PostFormTestCase(TestCase):
 
     def setUp(self):
+        self.url = reverse('feed')
         self.user = User.objects.create_user(
             '@johndoe',
             first_name = 'John',
@@ -38,3 +39,26 @@ class PostFormTestCase(TestCase):
     def test_form_has_necessary_fields(self):
         form = PostForm()
         self.assertIn('text', form.fields)
+
+
+    # def test_unsuccesful_post(self):
+    #     self.form_input['test'] = ''
+    #     before_count = Post.objects.count()
+    #     response = self.client.post(self.url, self.form_input)
+    #     after_count = Post.objects.count()
+    #     self.assertEqual(after_count, before_count)
+    #     self.assertEqual(response.status_code, 302)
+    #     self.assertTemplateUsed(response, 'feed.html')
+    #     form = response.context['form']
+    #     self.assertTrue(isinstance(form, PostForm))
+    #     self.assertTrue(form.is_bound)
+    #
+
+    # def test_succesful_post(self):
+    #     before_count = Post.objects.count()
+    #     response = self.client.post(self.url, self.form_input, follow = True)
+    #     # response_url = reverse('feed')
+    #     # self.assertRedirects(response, response_url, status_code = 302, target_status_code = 200)
+    #     # self.assertTemplateUsed(response, 'feed.html')
+    #     after_count = Post.objects.count()
+    #     self.assertEqual(after_count, before_count+1)
--- microblogs/views.py
+++ microblogs/views.py
@@ -29,6 +29,7 @@ def feed(request):
         if form.is_valid():
             Post.objects.create(author = logged_in_user, text = form.cleaned_data.get('text'))
             return redirect('feed')
+        messages.add_message(request, messages.ERROR, 'The provided post were invalid!')
     else:
         users = User.objects.all()
         form = PostForm()
