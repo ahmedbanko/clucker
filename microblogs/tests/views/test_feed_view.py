23acfa020f493735ff40b631631339fa2a81a6b8
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2021-11-10 15:47:26 +0000

    Add test for @login_required when get feed

--- microblogs/tests/views/test_feed_view.py
+++ microblogs/tests/views/test_feed_view.py
@@ -2,10 +2,14 @@
 from django.test import TestCase
 from django.urls import reverse
 from microblogs.forms import PostForm
+from microblogs.models import User
+from microblogs.tests.helpers import reverse_with_next
 
 
 class FeedViewTestCase(TestCase):
     """Tests of the feed view."""
+
+    fixtures = ['microblogs/tests/fixtures/default_user.json']
 
     def setUp(self):
         self.url = reverse('feed')
@@ -14,9 +18,16 @@ class FeedViewTestCase(TestCase):
         self.assertEqual(self.url,'/feed/')
 
     # def test_get_feed(self):
+    #     self.client.login(username='@johndoe', password='Password123')
     #     response = self.client.get(self.url)
     #     self.assertEqual(response.status_code, 200)
     #     self.assertTemplateUsed(response, 'feed.html')
     #     form = response.context['form']
     #     self.assertTrue(isinstance(form, PostForm))
     #     self.assertFalse(form.is_bound)
+
+
+    def test_get_feed_redirects_when_not_logged_in(self):
+        response = self.client.get(self.url)
+        redirect_url =  reverse_with_next('log_in', self.url)
+        self.assertRedirects(response, redirect_url, status_code=302, target_status_code=200)
