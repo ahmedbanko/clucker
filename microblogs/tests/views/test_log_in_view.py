218a610f0e8f5a665211fcebb48106d23eb7c419
Ahmed Banko <ahmed.banko@kcl.ac.uk>
2021-11-10 07:15:21 +0000

    Reorganise tests into views, forms, models

--- microblogs/tests/views/test_log_in_view.py
+++ microblogs/tests/views/test_log_in_view.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from django.urls import reverse
 from microblogs.forms import LogInForm
 from microblogs.models import User
-from .helpers import LogInTester
+from microblogs.tests.helpers import LogInTester
 from django.contrib import messages
 
 class LogInViewTestCase(TestCase, LogInTester):
